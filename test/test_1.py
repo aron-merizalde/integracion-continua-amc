e6e7cba0827a3ddd0bbf5f5bdb3ca211f88323d3
aron-merizalde <aron.merizalde@gmail.com>
2026-02-24 19:55:33 +0100

    Update test_1.py

--- test/test_1.py
+++ test/test_1.py
@@ -1,10 +1,7 @@
-def verificar_pares_impares(lista_numeros):
-    for numero in lista_numeros:
-        if numero % 2 == 0:
-            print(f"{numero} es par")
-        else:
-            print(f"{numero} es impar")
-
-# Ejemplo de uso
-mis_numeros = [10, 5, 8, 3, 17, 20]
-verificar_pares_impares(mis_numeros)
+def test_verificar_pares_impares():
+    numeros = [10, 5, 8, 3, 17, 20]
+    esperado = ["par", "impar", "par", "impar", "impar", "par"]
+    
+    resultado = verificar_pares_impares(numeros)
+    
+    assert resultado == esperado
