80011efacd10435a6f5cb5f9a022beb6cbab3f80
aron-merizalde <aron.merizalde@gmail.com>
2026-02-24 20:02:32 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,8 @@
-# Solicitar el número al usuario
-try:
-    numero = int(input("Introduce un número entero: "))
-
-    # Verificar paridad con el operador módulo %
-    if numero % 2 == 0:
-        print(f"{numero} es un número par.")
-    else:
-        print(f"{numero} es un número impar.")
-except ValueError:
-    print("Por favor, introduce un número entero válido.")
+def verificar_pares_impares(lista_numeros):
+    resultado = []
+    for numero in lista_numeros:
+        if numero % 2 == 0:
+            resultado.append("par")
+        else:
+            resultado.append("impar")
+    return resultado
